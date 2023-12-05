86e64549b3ce7f16fd2818ec44bbc80f83945be3
Steven Gort <steven.gort@ictu.nl>
2023-12-05 10:29:02 +0100

    initial config copied from original

--- js/config.js
+++ js/config.js
@@ -1,43 +1,68 @@
 let respecConfig = {
+  // Config parameters
   useLogo: true,
   useLabel: true,
-  license: "cc-by",
+  license: "cc0",
   specStatus: "DEF",
   specType: "HR",
-  pubDomain: "dk",
-  shortName: "template",
-  publishDate: "2023-06-21",
-  publishVersion: "0.0.3",
-  
-  previousPublishVersion: "0.0.2",
-  previousPublishDate: "2022-06-24",
+  pubDomain: "ronl",
+
+  // shortName = Repository name
+  shortName: "LegitiMaat",
+  publishDate: "2022-07-01",
+  publishVersion: "1.0.0",
+
+  previousPublishVersion: "",
+  previousPublishDate: "",
   previousMaturity: "WV",
-  title: "titleText",
+  title: "LegitiMaat",
+  subtitle: "Een werkmethode voor het doen van onderzoek door derden naar het gebruik van algoritmen door een overheidsorganisatie.",
+  
+  // Content
   content: {"ch01": "informative", "ch02": "", "mermaid": ""},
-  editors:
-    [
-      {
-        name: "Logius Standaarden",
-        company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
-      }
-    ],
-  authors:
-    [
-      {
-        name: "Logius Standaarden",
-        company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
-      }
-    ],
-  github: "https://github.com/Logius-standaarden/ReSpec-template",
+  editors: [{
+    name: "Mildo van Staden",
+    mailto: "Mildo.Staden@minbzk.nl",
+    company: "BZK",
+    companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+  }, {
+    name: "Marlies van Eck",
+    mailto: "marlies@hooghiemstra-en-partners.nl",
+    company: "Hooghiemstra & Partners",
+    companyURL: "https://hooghiemstra-en-partners.nl",
+  }],
+  authors: [{
+    name: "Marlies van Eck",
+    url: "https://hooghiemstra-en-partners.nl",
+  }, {
+    name: "Mariette Lokin",
+    url: "https://www.rijksoverheid.nl/ministeries/ministerie-van-financien",
+  }, {
+    name: "Maike Klip",
+    url: "https://www.nationaleombudsman.nl/",
+  }, {
+    name: "Giulia Bössenecker",
+    url: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+  }, {
+    name: "Carlijn Oldeman",
+    url: "https://hooghiemstra-en-partners.nl",
+  }, {
+    name: "Robert van Doesburg",
+    url: "https://www.tno.nl",
+  }, {
+    name: "Abram Klop",
+    url: "https://www.ubrijk.nl/service/i-interim-rijk",
+  }, {
+    name: "Steven Gort",
+    url: "https://www.ictu.nl",
+  }],
+  github: "https://github.com/MinBZK/LegitiMaat",
 
-
-  // Create PDF and link to file in header (optional):
-  alternateFormats: [
+    // Create PDF and link to file in header (optional):
+    alternateFormats: [
       {
           label: "pdf",
-          uri: "template.pdf",
+          uri: "LegitiMaat.pdf",
       },
   ],
 };
